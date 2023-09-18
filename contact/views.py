1d9a8a6c48e11f6e6b9c97ed8e21ca72dc513ed4
koykhaled <koykhaled@gmail.com>
2023-09-19 00:56:25 +0300

    gmail sender + contact

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Info
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 
 def sendMessage(request):
-    return HttpResponse("hello")
+    info = Info.objects.first()
+    if request.method == "POST":
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [email],
+        )
+        print(subject)
+    return render(request, 'contact/contact.html', {'info': info})
--- project/settings.py
+++ project/settings.py
@@ -143,3 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # LOGIN_REDIRECT_URL = '/jobs'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "koykhaled@gmail.com"
+EMAIL_HOST_PASSWORD = "ibux hvaf fccy cxlf"
+EMAIL_USE_TLS = True
+EMAIL_PORT = "587"
